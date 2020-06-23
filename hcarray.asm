4d480e7c44b167b00ec190e35e50553dfbe6e41a
dusanilic <idtronicsrs@gmail.com>
2020-06-23 17:14:57 +0200

    Added hcarray.asm, main.asm, hcarray.inc, structures.inc. First commit.

---
+++ hcarray.asm
@@ -0,0 +1,52 @@
+include Irvine.32.inc
+include hcarray.inc
+
+.code
+
+; //============================================================================
+ReadFile PROC,
+  Name:     PTR BYTE,      ; // Pointer to File Name
+  Content:  PTR BYTE,      ; // Pointer to File Content
+  Len:      BYTE           ; // Number of characters read from a file
+; //============================================================================
+LOCAL FileHandle: DWORD
+
+  push edx
+  push ecx
+  push eax
+
+  mov   edx, OFFSET Name
+  call  OpenInputFile
+  .IF eax == INVALID_HANDLE_VALUE
+    mov   edx, "Error occurred while opening file."
+    call  WriteString
+    jmp   end
+  .ELSE
+    mov   FileHandle, eax ; // File handle is stored in eax after calling OpenInputFile
+    mov   edx, OFFSET Content
+    mov   ecx, BUFFER_SIZE
+    mov   eax, FileHandle
+    call  ReadFromFile
+    jc    reading_error ; // Carry Flag is set if an error occurred while reading_error
+    mov   Len, eax
+    ; // Terminating string with "\0"
+    add   edx, Len
+    mov   BYTE PTR [edx], 0
+    ; // Closing the file
+    mov   eax, FileHandle
+    call  CloseFile
+    ; // Checking if reading was successful
+    mov edx, OFFSET Content
+    call WriteString
+  .ENDIF
+  reading_error:
+  mov   edx, OFFSET "Error occurred while reading file."
+  call  WriteString
+  end:
+  pop eax
+  pop ecx
+  pop edx
+
+ReadFile ENDP
+
+END
---
+++ main.asm
@@ -0,0 +1,49 @@
+.386
+.model flat, stdcall
+.stack 4096
+ExitProcess proto, dwExitCode:dword
+include Irvine.32.inc
+include hcarray.inc
+
+.data?
+  FileHandle        DWORD ?
+  FileContent       BYTE  BUFFER_SIZE DUP(?)
+  CharactersRead    BYTE  ? ; // Number of characters read from a file
+
+.data
+  FileName          BYTE  "example.txt",0
+  FileOpenErrMsg  BYTE  "Error occurred while opening file.", 0
+  FileReadErrMsg  BYTE  "Error occurred while reading file.", 0
+
+.code
+  main proc
+    mov   edx, OFFSET FileName
+    call  OpenInputFile
+    .IF eax == INVALID_HANDLE_VALUE
+      mov   edx, OFFSET FileOpenErrMsg
+      call  WriteString
+      jmp   end
+    .ELSE
+      mov   FileHandle, eax ; // File handle is stored in eax after calling OpenInputFile
+      mov   edx, OFFSET FileContent
+      mov   ecx, BUFFER_SIZE
+      mov   eax, FileHandle
+      call  ReadFromFile
+      jc    reading_error ; // Carry Flag is set if an error occurred while reading_error
+      mov   CharactersRead, eax
+      ; // Terminating string with "\0"
+      add   edx, CharactersRead
+      mov   BYTE PTR [edx], 0
+      ; // Closing the file
+      mov   eax, FileHandle
+      call  CloseFile
+      ; // Checking if reading was successful
+      mov edx, OFFSET FileContent
+      call WriteString
+    .ENDIF
+reading_error:
+  mov   edx, OFFSET FileReadErrMsg
+  call  WriteString
+end:
+  main endp
+END main
